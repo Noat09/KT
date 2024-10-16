9847c9e58e8893ca11206678605cfd06693108e3
Noat09 <tanthienvu8@gmail.com>
2024-10-16 16:42:37 +0700

    Login

---
+++ QLTrungTamTT/src/main/java/com/mycompany/qltrungtamtt/QLTrungTamTT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qltrungtamtt;
+
+/**
+ *
+ * @author PHAN CANH TOAN
+ */
+public class QLTrungTamTT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
